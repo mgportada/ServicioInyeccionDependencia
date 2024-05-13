1c1e5b1f861c7f29a761729448ec551124e88fe3
manugp <manuelgarces@iesportada.org>
2024-05-13 23:43:14 +0200

    Minor Change

--- ServicioInyeccionDependencia/Program.cs
+++ ServicioInyeccionDependencia/Program.cs
@@ -11,7 +11,7 @@ namespace ServicioInyeccionDependencia
             var communicationService = new CommunicationService();
 
             var customers = customerService.GetCustomers();
-            var message = "¡Hola! Su pedido ya está disponible para recoger. Gracias por usar nuestro servicio de correo electrónico";
+            var message = "¡Hola! Su pedido ya está disponible para recoger. Gracias por utilizar nuestro servicio de email";
             foreach (var customer in customers)
             {
                 communicationService.SendMessage(customer.Email, message);
